db911b2af75c20647ebbd176c9024da04b132d38
Hamza Asghar <hamzaraftar765@gmail.com>
2024-10-11 20:11:59 -0700

    making all middleware

---
+++ todo/server/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import pg from "pg";
+import cors from "cors";
+
+const app = express();
+const port = 5000;
+
+//Middleware
+app.use(cors())
+app.use(express.json()); 
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
