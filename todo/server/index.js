03bb71a679402ae6b880671f60d7888c744050f7
Hamza Asghar <hamzaraftar765@gmail.com>
2024-10-12 22:30:25 -0700

    making post route for todo

--- todo/server/index.js
+++ todo/server/index.js
@@ -5,9 +5,33 @@ import cors from "cors";
 const app = express();
 const port = 5000;
 
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "todo",
+  password: "12345",
+  port: "5432",
+});
+db.connect();
+
 //Middleware
-app.use(cors())
-app.use(express.json()); 
+app.use(cors());
+app.use(express.json());
+
+//Create todo
+
+app.post("/todo", async (req, res) => {
+  try {
+    const { title, description } = req.body;
+    const data = await db.query(
+      "INSERT INTO pern_todo (title,description) VALUES($1,$2) RETURNING *",
+      [title, description]
+    );
+    res.json(data.rows);
+  } catch (error) {
+    console.error(error.message);
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
