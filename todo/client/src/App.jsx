571e4cfa92c0525f9f35e136407d12b8a2ce0774
Hamza Asghar <hamzaraftar765@gmail.com>
2024-10-13 00:48:30 -0700

    post feature done in ui

--- todo/client/src/App.jsx
+++ todo/client/src/App.jsx
@@ -2,8 +2,8 @@ import Header from "./components/Header";
 import Input from "./components/Input";
 export default function App() {
   return (
-    <div className="max-w-2xl  h-full mx-auto m-20">
-      {/* <Header /> */}
+    <div className="max-w-4xl   mx-auto m-20   rounded-xl h-[700px] shadow-2xl">
+      <Header />
       <Input />
     </div>
   );
--- todo/client/src/components/Header.jsx
+++ todo/client/src/components/Header.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const Header = () => {
   return (
     <div>
-      <h1 className="text-center text-3xl font-bold text-blue-600">Todo</h1>
+      <h1 className="text-center text-3xl font-bold text-yellow-500 p-7">
+        Todo
+      </h1>
     </div>
   );
 };
