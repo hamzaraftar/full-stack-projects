72499891fc42727929bc058c13af54677e4e78a7
Hamza Asghar <hamzaraftar765@gmail.com>
2024-10-14 07:00:44 -0700

    delete feature is done

--- todo/client/src/App.jsx
+++ todo/client/src/App.jsx
@@ -3,7 +3,7 @@ import Input from "./components/Input";
 import Alltodo from "./components/Alltodo";
 export default function App() {
   return (
-    <div className="max-w-4xl   mx-auto m-20   rounded-xl h-[700px] shadow-2xl">
+    <div className="max-w-4xl   mx-auto m-20   rounded-xl h-auto shadow-2xl">
       <Header />
       <Input />
       <Alltodo />
--- todo/client/src/components/Alltodo.jsx
+++ todo/client/src/components/Alltodo.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { MdDeleteOutline } from "react-icons/md";
 
 const Alltodo = () => {
   const [value, setValue] = useState([]);
@@ -8,18 +9,28 @@ const Alltodo = () => {
     const data = await allTodo.data;
     setValue(data);
   }
-  console.log(value);
-
   useEffect(() => {
     allData();
   }, []);
+
+  async function handleDelete(id) {
+    try {
+      const deleteTodo = await axios.delete(`http://localhost:5000/todo/${id}`);
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
   return (
     <div className="mx-auto w-[400px] p-10">
       <div>
         {value.map((todo) => (
           <div key={todo.id}>
             <h1 className="text-xl text-center font-semibold">{todo.title}</h1>
-            <p className="font-serif text-center">{todo.description}</p>
+            <MdDeleteOutline
+              onClick={() => handleDelete(todo.id)}
+              className="text-3xl cursor-pointer hover:text-red-500 hover:bg-slate-300 rounded-full mx-2"
+            />
+            <p className="font-serif text-center ">{todo.description}</p>
           </div>
         ))}
       </div>
