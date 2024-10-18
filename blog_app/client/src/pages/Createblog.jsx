9f6d37999832acccfd897888ef3c90a1ec8b17b8
Hamza Asghar <hamzaraftar765@gmail.com>
2024-10-18 22:21:50 +0500

    adding post

--- blog_app/client/src/pages/Createblog.jsx
+++ blog_app/client/src/pages/Createblog.jsx
@@ -1,14 +1,48 @@
+import { useState } from "react";
+import axios from "axios";
+
 function Createblog() {
+  const [value, setValur] = useState({
+    title: "",
+    description: "",
+  });
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    console.log(value);
+
+    try {
+      const postData = await axios.post("http://localhost:5000/blogs", value);
+      const data = await postData.data;
+      console.log(data);
+
+      if (data) {
+        setValur({
+          title: "",
+          description: "",
+        });
+      }
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
   return (
     <div>
-      <form className="flex flex-col m-5 min-w-[400px] mx-auto md:w-[600px] sm:w-[500px] ">
+      <form
+        className="flex flex-col m-5 min-w-[400px] mx-auto md:w-[600px] sm:w-[500px] "
+        onSubmit={handleSubmit}
+      >
         <input
+          value={value.title}
+          onChange={(e) => setValur({ ...value, title: e.target.value })}
           type="text"
           placeholder="Enter title"
           required
           className="outline-none font-medium text-xl border-2 border-blue-500 rounded-md m-2 p-2"
         />
         <input
+          value={value.description}
+          onChange={(e) => setValur({ ...value, description: e.target.value })}
           type="text"
           placeholder="Enter description"
           required
