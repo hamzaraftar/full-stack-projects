04788230d85d88d3505cc62ac25d78a9087097b7
Hamza Asghar <hamzaraftar765@gmail.com>
2024-10-19 17:04:42 +0500

    add hambargar in ui

--- blog_app/client/src/components/Header.jsx
+++ blog_app/client/src/components/Header.jsx
@@ -1,7 +1,9 @@
 import { Link } from "react-router-dom";
-
+import { RiMenu3Fill } from "react-icons/ri";
+import { useState } from "react";
 
 const Header = () => {
+  const [show, setShow] = useState(false);
   return (
     <div className="mt-10 mx-10">
       <div className="mt-10 sm:flex items-center justify-between mx-10 hidden">
@@ -15,6 +17,27 @@ const Header = () => {
           Create Blog
         </Link>
       </div>
+      <RiMenu3Fill
+        className="text-3xl sm:hidden"
+        onClick={() => setShow(!show)}
+      />
+      <ul
+        className={show ? `flex flex-col justify-center gap-10 mt-7` : `hidden`}
+      >
+        <li>
+          <Link className="font-semibold text-2xl" to={"/"}>
+            Blogs
+          </Link>
+        </li>
+        <li>
+          <Link
+            className=" bg-gradient-to-r from-sky-500 to-indigo-500  rounded-md text-white p-2 hover:bg-gradient-to-r hover:from-sky-600 hover:to-indigo-600 text-lg"
+            to={"/create-blog"}
+          >
+            Create Blog
+          </Link>
+        </li>
+      </ul>
     </div>
   );
 };
