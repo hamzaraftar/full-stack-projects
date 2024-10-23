67c3c1576316b04599d43c5015d32c4884d1887d
Hamza Asghar <hamzaraftar765@gmail.com>
2024-10-23 06:02:50 -0700

    adding delete feature

--- blog_app/client/src/pages/Home.jsx
+++ blog_app/client/src/pages/Home.jsx
@@ -1,16 +1,20 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { MdDelete } from "react-icons/md";
 
 export default function Home() {
   const [data, setData] = useState([]);
   async function fetchAllToDo() {
     const allblogs = await axios.get("http://localhost:5000/blogs");
     setData(allblogs.data);
-    console.log(data);
   }
   useEffect(() => {
     fetchAllToDo();
-  }, []);
+  }, [data]);
+
+  async function handleDelete(id) {
+    const deleteBlogs = await axios.delete(`http://localhost:5000/blogs/${id}`);
+  }
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-12  mx-10 mt-10">
       {data.map((item) => (
@@ -18,8 +22,12 @@ export default function Home() {
           key={item.id}
           className="hover:bg-slate-100 rounded-xl p-2 cursor-pointer px-2 "
         >
-          <h2 className="font-semibold text-2xl">{item.title}</h2>
-          <p className="font-medium tect">{item.description}</p>
+          <h2 className="font-semibold text-2xl truncate">{item.title}</h2>
+          <p className="font-medium tect mt-4 truncate">{item.description}</p>
+          <MdDelete
+            onClick={() => handleDelete(item.id)}
+            className="text-3xl  rounded-full hover:bg-red-300 "
+          />
         </div>
       ))}
     </div>
