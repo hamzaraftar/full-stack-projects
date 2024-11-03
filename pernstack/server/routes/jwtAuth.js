96181bc3bbfb4b19f5c79debbc222b94aaaa0de7
Hamza Asghar <hamzaraftar765@gmail.com>
2024-11-03 07:47:32 -0800

    register fix

--- pernstack/server/routes/jwtAuth.js
+++ pernstack/server/routes/jwtAuth.js
@@ -7,11 +7,18 @@ import db from "../db.js";
 router.post("/register", async (req, res) => {
   try {
     const { name, email, password } = req.body;
-    const newUser = await db.query(
-      "INSERT INTO user (user_name , user_email, user_password)   VALUES ($1 , $2 , $2) RETURING *",
+    const userMail = await db.query(
+      "SELECT * FROM users WHERE user_email = $1 ",
+      [email]
+    );
+    if (userMail.rows.length !== 0) {
+      return res.status(401).send("User already exist");
+    }
+    const data = await db.query(
+      "INSERT INTO users (user_name , user_email, user_password)   VALUES ($1 , $2 , $2) RETURNING *",
       [name, email, password]
     );
-    res.json(newUser.rows[0]);
+    res.json(data.rows[0]);
   } catch (error) {
     console.error(error);
     res.status(500).send("Server error");
