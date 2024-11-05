c96d23def2edce1bb5aef892c6504807590fd139
Hamza Asghar <hamzaraftar765@gmail.com>
2024-11-05 05:42:49 -0800

    client complete

--- pernstack/client/src/App.jsx
+++ pernstack/client/src/App.jsx
@@ -3,12 +3,14 @@ import { Routes, Route, redirect } from "react-router-dom";
 import Dashbord from "./components/Dashbord";
 import Register from "./components/Register";
 import Login from "./components/Login";
+import { useState } from "react";
 
 function App() {
+  const [isAuthenticated , setIsAuthenticated]  = useState(false)
   return (
     <>
       <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={  <Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashbord" element={<Dashbord />} />
       </Routes>
