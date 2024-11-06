e454348cc64c2cd817b741cc2c10a6b07e986600
Hamza Asghar <hamzaraftar765@gmail.com>
2024-11-06 05:05:30 -0800

    lintree

---
+++ next/src/app/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <nav className=" bg-white">
+      <div>fsdfsdfsf</div>
+    </nav>
+  );
+}
---
+++ next/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Navbar from "./components/Navbar";
+export default function Home() {
+  return (
+    <main>
+      <section className=" bg-green-600">
+        <Navbar />
+      </section>
+    </main>
+  );
+}
