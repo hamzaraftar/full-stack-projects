e20d89629553a4fb327e9767d7209a9639a76226
Hamza Asghar <hamzaraftar765@gmail.com>
2024-11-06 05:35:25 -0800

    navbar

--- next/src/app/page.tsx
+++ next/src/app/page.tsx
@@ -2,7 +2,7 @@ import Navbar from "./components/Navbar";
 export default function Home() {
   return (
     <main>
-      <section className=" bg-green-600">
+      <section className=" bg-green-700 min-h-screen">
         <Navbar />
       </section>
     </main>
