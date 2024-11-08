8712223b00a2776aa72cfb3aeb94a0fe27f09861
Hamza Asghar <hamzaraftar765@gmail.com>
2024-11-08 06:03:29 -0800

    flask

--- flask/backend/app.py
+++ flask/backend/app.py
@@ -6,3 +6,6 @@ app =  Flask(__name__)
 CORE(app)
 app.config['SQLALCHEMY']  = "sqlite:///friend.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+if __name__ == "__main__":
+  app.run(debug=True)
