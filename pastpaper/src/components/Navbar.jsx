1ffb7de5ccf747cdb4dd6c549181eb1f57add341
Hamza Asghar <hamzaraftar765@gmail.com>
2024-11-13 04:55:06 -0800

    fix bug

--- pastpaper/src/components/Navbar.jsx
+++ pastpaper/src/components/Navbar.jsx
@@ -10,13 +10,13 @@ export default function Navbar() {
       </div>
       <ul className="flex justify-center items-center gap-4 font-semibold">
         <Link href={"/"}>
-          <li className="hover:bg-slate-300 p-3 py-2  rounded-xl">Home</li>
+          <li className="hover:bg-slate-300 p-3 py-2  rounded-xl ">Home</li>
         </Link>
         <Link href={"/about"}>
-          <li className="hover:bg-slate-300 p-3 py-2  rounded-xl">About</li>
+          <li className="hover:bg-slate-300 p-3 py-2  rounded-xl ">About</li>
         </Link>
         <Link href={"/contact"}>
-          <li className="hover:bg-slate-300 p-3 py-2  rounded-xl">
+          <li className="hover:bg-slate-300 p-3 py-2  rounded-xl ">
             Contact Us
           </li>
         </Link>
