6fa5c7a03235ec639044ff79c8e790575abca242
Hamza Asghar <hamzaraftar765@gmail.com>
2024-12-23 10:05:27 -0800

    jwt auth section is complete

--- fullauth/server/routes/auth.js
+++ fullauth/server/routes/auth.js
@@ -6,15 +6,23 @@ const jwtutil = require("../utils/jwt");
 router.get("/", (req, res) => {
   res.send("auth  home ");
 });
+router.post("/verify", async (req, res) => {
+  let verified = await jwtutil.verify(req.body.token);
+  res.json({ "data": verified });
+});
 
 router.post("/login", async (req, res) => {
   try {
-    const result = await client.query("SELECT * FROM users");
+    const result = await client.query(
+      "SELECT * FROM users  WHERE user_email = $1",
+      [req.body.user_email]
+    );
     if (result.rows.length > 0) {
+      delete result.rows[0].user_password;
       let authToken = await jwtutil.create(result.rows[0]);
-      res.json({ token: authToken });
+      res.json({ "token": authToken });
     } else {
-      res.json({ error: "password or email is incorrect" });
+      res.status(401).json({ error: "password or email is incorrect" });
     }
   } catch (error) {
     console.error(error.message);
