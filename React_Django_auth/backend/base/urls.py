5c7445370a71f3b3ff4ce1702dc1568e9eb754b3
Hamza Asghar <hamzaraftar765@gmail.com>
2025-02-19 00:18:16 -0800

    adding jwt cookie

--- React_Django_auth/backend/base/urls.py
+++ React_Django_auth/backend/base/urls.py
@@ -1,14 +1,14 @@
 from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
     TokenRefreshView,
 )
 from django.urls import path
-from .views import get_notes
+from .views import get_notes,MyTokenObtainPairSerializer
+
 
 
 urlpatterns = [
 
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/', MyTokenObtainPairSerializer.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('notes/', get_notes),
  
