12e8775415295cfbcdaf9a5cc7eb2c47bdacec88
Hamza Asghar <hamzaraftar765@gmail.com>
2025-03-01 10:05:05 -0800

    every thing i working fine

--- Django_React/frontend/src/pages/Login.jsx
+++ Django_React/frontend/src/pages/Login.jsx
@@ -1,4 +1,4 @@
 import Form from "../components/Form";
 export default function Login() {
-  return <Form route="/api/token/" method="login" />;
+  return <Form route="http://127.0.0.1:8000/api/token/" method="login" />;
 }
