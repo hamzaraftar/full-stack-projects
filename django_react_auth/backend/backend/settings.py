8501a0ece87394cb07a604ff6b1dd49cc100bd19
Hamza Asghar <hamzaraftar765@gmail.com>
2025-03-06 16:38:35 -0800

    backend complete

--- django_react_auth/backend/backend/settings.py
+++ django_react_auth/backend/backend/settings.py
@@ -58,7 +58,7 @@ MIDDLEWARE = [
 REST_FRAMEWORK = {    
     'DEFAULT_AUTHENTICATION_CLASSES': (
         
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'base.authentication.JWTAuthenticationCookie', 
     )  ,
      'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
