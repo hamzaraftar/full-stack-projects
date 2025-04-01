54b2a80a6b820ebde9118e14193f09fd2f9b3ef3
Hamza Asghar <hamzaraftar765@gmail.com>
2025-04-01 09:55:37 +0500

    removeing and imporving app

--- todo_full_auth/frontend/src/pages/Home.jsx
+++ todo_full_auth/frontend/src/pages/Home.jsx
@@ -4,7 +4,12 @@ import { Link } from "react-router-dom";
 export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center h-screen bg-gray-100 text-gray-800">
-      <h1 className="text-4xl mb-3">Wellcome to Todo app </h1>
+      <h1 className="text-4xl mb-3 ">
+        Wellcome to
+        <span className="relative after:content-[''] after:block after:w-full after:h-[5px] after:bg-gradient-to-r after:from-blue-500 after:via-purple-500 after:to-pink-500 after:absolute after:left-0 after:bottom-0 after:rounded-full">
+          Todo app
+        </span>
+      </h1>
       <h2 className="text-xl">
         <Link to="/login" className="text-blue-500   hover:text-blue-700">
           Login
