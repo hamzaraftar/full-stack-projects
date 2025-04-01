e47455b04ae3aee6d6d35555cd48de6ae31e89f1
Hamza Asghar <hamzaraftar765@gmail.com>
2025-04-01 11:53:15 +0500

    signals in django

--- todo_full_auth/frontend/src/components/LoadingIndicator.jsx
+++
@@ -1,7 +0,0 @@
-export default function LoadingIndicator() {
-  return (
-    <div className="flex justify-center">
-      <div className="w-6 h-6 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
-    </div>
-  );
-}
