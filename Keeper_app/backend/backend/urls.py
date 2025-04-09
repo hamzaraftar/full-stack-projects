41924eb519b41c34d6b4b3b948cb832785b07c3d
Hamza Asghar <hamzaraftar765@gmail.com>
2025-04-09 21:28:55 +0500

    jwt work fine

--- Keeper_app/backend/backend/urls.py
+++ Keeper_app/backend/backend/urls.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
 from django.urls import path
+from app import views
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # Uesr
+    path('create-user/', views.create_user, name='create_user'),
+    path('user-info/', views.user_info, name='user_info'),
+
+
+
+
+    # Tokens
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),    
 ]
