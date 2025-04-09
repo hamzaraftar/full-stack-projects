67bca8e62a293cd6888ba1e7389747e65560dedc
Hamza Asghar <hamzaraftar765@gmail.com>
2025-04-09 21:20:41 +0500

    making models

--- Keeper_app/backend/app/views.py
+++ Keeper_app/backend/app/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view , permission_classes
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny,IsAuthenticated
+from rest_framework import status
+from .serializers import UserSerializer
+from .models import User
 
-# Create your views here.
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def create_user(request):
+    if request.method == 'POST':
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def user_info(request):
+    serializer = UserSerializer(request.user)
+    return Response(serializer.data)
