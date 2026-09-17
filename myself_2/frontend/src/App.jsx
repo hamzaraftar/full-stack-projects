f9b8f8265d53338a8e9b43937304e9c32dd227c8
Hamza Asghar <hamzaraftar765@gmail.com>
2026-09-17 17:18:54 +0500

    project complete

--- myself_2/frontend/src/App.jsx
+++ myself_2/frontend/src/App.jsx
@@ -4,6 +4,9 @@ import axios from "axios";
 
 function App() {
   const [todo, setTodo] = useState([]);
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [editId, setEditId] = useState(null);
 
   useEffect(() => {
     const fetchTodos = async () => {
@@ -25,6 +28,53 @@ function App() {
       console.error(error.message);
     }
   };
+
+  const handleSubmit = async () => {
+    if (!title || !content) {
+      return;
+    }
+
+    try {
+      if (editId !== null) {
+        await axios.put(`http://127.0.0.1:8000/api/todo/${editId}/`, {
+          title: title,
+          content: content,
+        });
+
+        setTodo(
+          todo.map((item) =>
+            item.id === editId
+              ? { ...item, title: title, content: content }
+              : item,
+          ),
+        );
+
+        setEditId(null);
+      } else {
+        const response = await axios.post("http://127.0.0.1:8000/api/todo/", {
+          title: title,
+          content: content,
+        });
+        setTodo([...todo, response.data]);
+      }
+      setTitle("");
+      setContent("");
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  const handleEdit = async (item) => {
+    setEditId(item.id);
+    setTitle(item.title);
+    setContent(item.content);
+  };
+
+  const handleCancel = () => {
+    setEditId(null);
+    setTitle("");
+    setContent("");
+  };
   return (
     <>
       <div className="bg-gray-200 min-h-screen  pt-4">
@@ -36,18 +86,33 @@ function App() {
         <div className="todo main  min-h-screen  mx-auto rounded-2xl  mt-3">
           <div className="flex flex-col justify-center items-center gap-4 pt-7">
             <input
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
               type="text"
               placeholder="Enter Title"
-              className="border border-gray-300 rounded-md py-2 px-4 focus:outline-none focus:ring-2 focus:ring-blue-500 w-120 outline-none"
+              className=" text-md font-semibold  border border-gray-300 rounded-md py-2 px-4 focus:outline-none focus:ring-2 focus:ring-blue-500 w-120 outline-none"
             />
             <input
+              value={content}
+              onChange={(e) => setContent(e.target.value)}
               type="text"
               placeholder="Enter Content ..."
-              className="border border-gray-300 rounded-md py-2 px-4 focus:outline-none focus:ring-2 focus:ring-blue-500 w-120 outline-none"
+              className=" text-md font-semibold  border border-gray-300 rounded-md py-2 px-4 focus:outline-none focus:ring-2 focus:ring-blue-500 w-120 outline-none"
             />
-            <button className="w-96 bg-blue-500 hover:bg-blue-700 text-gray-200 font-bold py-2 px-4 rounded cursor-pointer">
-              Add Todo
+            <button
+              onClick={handleSubmit}
+              className="w-96 bg-blue-500 hover:bg-blue-700 text-gray-200 font-bold py-2 px-4 rounded cursor-pointer"
+            >
+              {editId !== null ? "EditTodo" : "Add Todo"}
             </button>
+            {editId && (
+              <button
+                onClick={handleCancel}
+                className="w-96 bg-gray-600 hover:bg-gray-700 text-gray-200 font-bold py-2 px-4 rounded cursor-pointer"
+              >
+                Cancel
+              </button>
+            )}
           </div>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-8">
             {todo.map((item) => (
@@ -55,12 +120,15 @@ function App() {
                 key={item.id}
                 className="bg-white shadow-md rounded-lg p-4 m-2"
               >
-                <h2 className="text-xl font-bold text-gray-800">
+                <h2 className="text-xl font-semibold text-gray-800">
                   {item.title}
                 </h2>
                 <p className="text-gray-600">{item.content}</p>
                 <div className="flex justify-between mt-">
-                  <button className="cursor-pointer bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4">
+                  <button
+                    onClick={() => handleEdit(item)}
+                    className="cursor-pointer bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4"
+                  >
                     Edit
                   </button>
                   <button
